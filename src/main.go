1d4e50c5de574d28f3c76b5e862e2736e0b0d526
Carlos N. Trejo <CarlosTrejo2308@users.noreply.github.com>
2021-05-12 19:06:36 -0500

    Testing get requests

--- src/main.go
+++ src/main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"io/ioutil"
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello")
+	resp, err := http.Get("https://api.github.com/zen")
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	//Convert the body to type string
+	sb := string(body)
+	log.Printf(sb)
+
 }
